1d2016eb66819403568cb9601713c2d911d946b0
Whit Sundby <wksundby@gmail.com>
2021-08-04 12:18:43 -0700

    adds Upload Photo layout and event type spinner to scrollable view

--- app/src/main/java/com/sundbean/raise/CreateEventActivity.kt
+++ app/src/main/java/com/sundbean/raise/CreateEventActivity.kt
@@ -2,10 +2,36 @@ package com.sundbean.raise
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
+import android.widget.Spinner
 
-class CreateEventActivity : AppCompatActivity() {
+class CreateEventActivity : AppCompatActivity(), AdapterView.OnItemSelectedListener {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_create_event)
+
+        val eventTypeSpinner : Spinner = findViewById(R.id.spinnerEventType)
+        // Create an ArrayAdapter using the string array and a default spinner layout
+        ArrayAdapter.createFromResource(
+            this,
+            R.array.event_type_array,
+            android.R.layout.simple_spinner_item
+        ).also { adapter ->
+            // Specify layout to use when the list of choices appears
+            adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+            // Apply the adapter to the spinner
+            eventTypeSpinner.adapter = adapter
+        }
+    }
+
+    override fun onItemSelected(parent: AdapterView<*>, view: View?, pos: Int, id: Long) {
+        // An item was selected. You can retrieve the selected item with
+        // parent.getItemAtPosition(pos)
+    }
+
+    override fun onNothingSelected(parent: AdapterView<*>) {
+        // Another interface callback
     }
 }
