443837b8217e6acf4bfd46cf1a078e2982a5ea46
Whit Sundby <wksundby@gmail.com>
2021-08-05 14:04:16 -0700

    creates set location activity layout

---
+++ app/src/main/java/com/sundbean/raise/RegistrationSetLocationActivity.kt
@@ -0,0 +1,11 @@
+package com.sundbean.raise
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class RegistrationSetLocationActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_registration_set_location)
+    }
+}
