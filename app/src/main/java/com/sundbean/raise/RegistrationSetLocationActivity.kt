e5519519aa1072b4744cac975ccf8821451e5727
Whit Sundby <wksundby@gmail.com>
2021-08-05 18:46:46 -0700

    adds autocomplete fragment and places sdk setup

--- app/src/main/java/com/sundbean/raise/RegistrationSetLocationActivity.kt
+++ app/src/main/java/com/sundbean/raise/RegistrationSetLocationActivity.kt
@@ -1,11 +1,55 @@
 package com.sundbean.raise
 
+import android.content.ContentValues.TAG
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.fragment.app.Fragment
+import com.google.android.gms.common.api.Status
+import com.google.android.libraries.places.api.Places
+import com.google.android.libraries.places.api.model.Place
+import com.google.android.libraries.places.widget.AutocompleteSupportFragment
+import com.google.android.libraries.places.widget.listener.PlaceSelectionListener
+import java.util.*
 
 class RegistrationSetLocationActivity : AppCompatActivity() {
+
+    private lateinit var autocompleteFragment : AutocompleteSupportFragment
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_registration_set_location)
+
+        // grab the user id and email id from the intent that sent you over to this page (from create account activity)
+        val userId = intent.getStringExtra("user_id")
+        val emailId = intent.getStringExtra("email_id")
+
+        // Initialize the AutocompleteSupportFragment.
+        autocompleteFragment =
+            supportFragmentManager.findFragmentById(R.id.autocomplete_fragment)
+                    as AutocompleteSupportFragment
+
+        // Specify the types of place data to return.
+        autocompleteFragment.setPlaceFields(listOf(Place.Field.ID, Place.Field.NAME))
+
+        // Set up a PlaceSelectionListener to handle the response.
+        autocompleteFragment.setOnPlaceSelectedListener(object : PlaceSelectionListener {
+            override fun onPlaceSelected(place: Place) {
+                // TODO: Get info about the selected place.
+                Log.i(TAG, "Place: ${place.name}, ${place.id}")
+            }
+
+            override fun onError(status: Status) {
+                // TODO: Handle the error.
+                Log.i(TAG, "An error occurred: $status")
+            }
+        })
+
+        //TODO: find a way to hide this api key!
+        Places.initialize(applicationContext, "AIzaSyB_A0RKs7JmFRfMvokjaUYPnDvciHNyheU")
+        val placesClient = Places.createClient(this)
     }
+
+
+
 }
